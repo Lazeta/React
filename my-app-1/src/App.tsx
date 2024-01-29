0b18e3e2720e5ee4b8c8cff595c617a18a7d526c
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2024-01-30 00:21:15 +0300

    create first function on react app and his called

--- my-app-1/src/App.tsx
+++ my-app-1/src/App.tsx
@@ -1,26 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { allowedNodeEnvironmentFlags } from 'process';
 
+
+// function declaration
 function App() {
+  // Полезное что-то
+ 
+  // обязана вернуть JSX
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is App component
+      {/* каждая функция обязана быть написана с большой буквы */}
+      <Rating/>
+      <Accordion/>
+      <Star/>
     </div>
   );
+}
+
+function Star() {
+  return (
+    <div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
+    </div>
+  )
+}
+
+function Rating() {
+  return (
+    <div>
+      <div>start</div>
+      <div>start</div>
+      <div>start</div>
+      <div>start</div>
+      <div>start</div>
+    </div>
+  )
+}
+
+function Accordion() {
+  return <div>
+    <h3>Menu</h3>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  </div>
 }
 
 export default App;
