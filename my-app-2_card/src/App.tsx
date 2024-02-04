270dee8ea1c8a35d9b5db0a3623d5f991ac88a34
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2024-02-04 18:54:46 +0300

    save progress

--- my-app-2_card/src/App.tsx
+++ my-app-2_card/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import styled from 'styled-components';
+import { BtnSave } from './components/styles/Buttons/BtnSave.styled';
+
 
 function App() {
   return (
     <div className='App'>
       <Box>
-        
+        <BtnSave/>
       </Box>
     </div>
   );
@@ -16,5 +17,13 @@ function App() {
 export default App;
 
 const Box = styled.div`
-  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: ${props => props.theme.colors.theme};
+
+  button {
+    cursor: pointer;
+  }
 `
---
+++ my-app-2_card/src/components/styles/CoverCard.styled.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+
+export const CoverCard = styled.img`
+    
+`
