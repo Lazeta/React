82ca34f89137b780b3a357a90fe97be26e914260
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2024-06-14 23:53:49 +0300

    start association array task

---
+++ my-app-4-onclick-for-menu/src/App.tsx
@@ -0,0 +1,23 @@
+const App = () => {
+  return (
+    <div className="bg-gray-800 p-4">
+      <header>
+        <nav>
+          <ul className="p-1">
+            <li>Menu 1</li>
+            <li>Menu 2</li>
+            <li>Menu 3</li>
+            <li>Menu 4</li>
+          </ul>
+        </nav>
+      </header>
+
+      <h1 className="text-2xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+      <p className="text-gray-700 mt-2">
+        Welcome to your React app with Tailwind CSS.
+      </p>
+    </div>
+  );
+};
+
+export default App;
